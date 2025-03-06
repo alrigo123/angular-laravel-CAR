acd9d59b958c6e20271dad938871fa1c2465fb1f
alrigo123 <alexestrada2804@gmail.com>
2025-03-06 14:39:52 -0500

    server2

--- notes laravel.sh
+++ notes laravel.sh
@@ -1,13 +1,14 @@
 ## Create api or new laravel app
 - laravel new "api_project"
 
-# *Excecute migrations to ensure that DB is configured
+# *Excecute migrations to ensure that DB is configured*
 - php artisan migrate
 
-## **CHECK DATABASE CONNECTION**
+## *CHECK DATABASE CONNECTION*
 > Create path route
     > Edita routes/web.php y agrega esta ruta temporal:
 
+'''
 use Illuminate\Support\Facades\DB;
 Route::get('/test-db', function () {
     try {
@@ -17,17 +18,18 @@ Route::get('/test-db', function () {
         return "Error al conectar a la base de datos: " . $e->getMessage();
     }
 });
+'''
 
-## ** GET products**
-1. Crear el Modelo y Migración
+##  GET products
+1. *Crear el Modelo y Migración*
 - php artisan make:model Product -m
-  
-    * Un modelo: app/Models/Product.php 
-    * Una migración: database/migrations/xxxx_xx_xx_xxxxxx_create_products_table.php]
+* Un modelo: app/Models/Product.php
+* Una migración: database/migrations/xxxx_xx_xx_xxxxxx_create_products_table.php
 
-2. Definir la Tabla en la Migración
+2. *Definir la Tabla en la Migración*
 * Edita database/migrations/xxxx_xx_xx_xxxxxx_create_products_table.php y define la estructura:
 
+'''
 public function up():
     {
         Schema::create('products', function (Blueprint $table) {
@@ -38,13 +40,15 @@ public function up():
             $table->timestamps();
         });
     }
+'''
 
 * Ejecutar migración
 - php artisan migrate
 
-3. Definir el Modelo
+3. *Definir el Modelo*
 * Edita app/Models/Product.php y agrega:
 
+'''
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -56,12 +60,13 @@ class Product extends Model
 
     protected $fillable = ['description', 'price', 'stock'];
 }
+'''
 
-4. Crear un Controlador
+4. *Crear un Controlador*
 - php artisan make:controller ProductController
-
 * Abre app/Http/Controllers/ProductController.php y agrega:
 
+'''
 namespace App\Http\Controllers;
 
 use App\Models\Product;
@@ -74,17 +79,17 @@ class ProductController extends Controller
         return response()->json(Product::all(), 200);
     }
 }
+'''
 
 -------
-******If api.php doesnt exist then use
+**** If api.php doesnt exist then use
 **** - php artisan install:api
 -------
 
 5. Definir la Ruta en api.php
 * routes/api.php y agrega:
 
+'''
 use App\Http\Controllers\ProductController;
-
 Route::get('/products', [ProductController::class, 'index']);
-
-[] Hacer lo login,register con tokens
+'''
--- notes.sh
+++ notes.sh
@@ -1,2 +1,8 @@
 [] Hacer un CRUD con login y authorizaciones ("este sera el de CRUD de cars")
+[] Primero logica crud de la tabla car en el server
+[] Intefaz del CRUD con angular y consumir api
+[] Crear o añadir tabla o migraciones (nose) de "usuarios"
+[] login y register, autorizaciones en rutas ?
+[] session como del app de control
+[] del frontend hacer lo que corresponde
 
